27806e24956b7eae97746679727195dbd265bb0d
Luiz Hanauer <luizhanauer@gmail.com>
2026-02-24 13:25:04 -0300

    fix(sw): ignorar esquemas de url nao suportados para prevenir erros no cache put

--- sw.js
+++ sw.js
@@ -37,6 +37,8 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
 
+  if (!event.request.url.startsWith('http')) return;
+
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       const fetchPromise = fetch(event.request).then((networkResponse) => {
